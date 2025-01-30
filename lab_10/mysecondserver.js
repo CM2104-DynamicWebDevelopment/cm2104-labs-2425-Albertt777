b479df5ecb629f98bbb124562f357b2f8b52d879
Albertt777 <ampadualbertsomuah@gmail.com>
2025-01-30 11:13:51 +0000

    ross change

---
+++ lab_10/mysecondserver.js
@@ -0,0 +1,9 @@
+var http = require ('http');
+var ourrentdata = require ('.mymodule');
+
+http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The data end time are ourrently: "+ ourrentdata.myDateTime());      
+    res.end('Hellow World!') ; 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                 ")
+}) .listen(8080);
